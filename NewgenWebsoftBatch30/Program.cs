ce9a835d5ce49d74a86936ca08d6a5ef70435bb2
kotwal345 <rushikeshkotwal345@gmail.com>
2025-06-08 11:44:16 +0530

    Update Connectionstring Name

--- NewgenWebsoftBatch30/Program.cs
+++ NewgenWebsoftBatch30/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<NewgenWebsoftDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("NewgenWebsoftConnectionString"))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 var app = builder.Build();
